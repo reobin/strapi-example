a732d4a69891739a160fc576139a22f28c2b722c
Robin Gagnon <me@reobin.dev>
2021-10-07 08:29:14 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '453864cf6d632c274bbed31405aafd93'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b90f88ba-396d-491b-a993-db6245f4d8ce'
+};
